be682f0405e053b7aff2ca9a33d6c6170eef78a0
Giovanni Souza <GSMartins999>
2023-10-12 17:26:34 -0300

    localStrageeUseEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App() {
   // Estado para armazenar a lista de compras
@@ -16,6 +16,53 @@ export default function App() {
     }
   };
 
+
+// Criando um botão para salvar no LocalStrorage
+  // const salvarLista = () => {
+  //   //transformando a lista em uma string
+  //   const listaString = JSON.stringify(listaCompras)
+  //   //armazenando a string das listas no localStrorage
+  //   localStorage.setItem("lista",listaString)
+  // }
+
+
+  //Usamos o useEffect para armazenar a lista de forma automática(Com ele n precisamos de botão)
+  useEffect(() => {
+    //É necessário criar uma condição para o useEffect conseguir pegar nossa lista salva
+    if(listaCompras.length > 0){
+    const listaString = JSON.stringify(listaCompras)
+    //armazenando a string das listas no localStrorage
+    localStorage.setItem("lista",listaString)}
+  }, [listaCompras])
+
+
+
+  // const pegarLista = () => {
+  //   //primeiro temos que criar uma váriavel para armazenar a lista de compras. 
+  //   //Depois transformamos ela em array novamente com o Json.parse
+  //   const listaSalva = JSON.parse(localStorage.getItem("lista"));
+  //   //Aqui setamos a lista no nosso estado
+  //   setListaCompras(listaSalva)
+  // }
+
+
+  //Vamos automatizar o pegar lista com o useEffect
+  useEffect(() => {
+    if(listaCompras){
+    const listaSalva = JSON.parse(localStorage.getItem("lista"));
+    //Precisamos fazer uma verificação para checar se existe algo dentro da listaSalva
+    if(listaSalva){
+      //Aqui setamos a lista no nosso estado
+      setListaCompras(listaSalva)
+    }
+  }
+  },[])
+
+
+  const removerItem = () => {
+    localStorage.removeItem("lista")
+    setListaCompras([])
+  }
   return (
     <div>
       <h1>Lista de Compras</h1>
@@ -26,6 +73,8 @@ export default function App() {
         placeholder="Digite um item"
       />
       <button onClick={adicionarItem}>Adicionar</button>
+      <button onClick={removerItem}>Remover</button>
+      {/* <button onClick={pegarLista}>Pegar Lista</button> */}
 
       <ul>
         {listaCompras.map((compra, index) => (
